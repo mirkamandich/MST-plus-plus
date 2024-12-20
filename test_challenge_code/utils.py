29dc84223ce1a68869c60335867acdfeafe4d45a
CKTraynor <ctrayno2@vols.utk.edu>
2024-12-20 14:47:53 -0500

    Update utils.py

--- test_challenge_code/utils.py
+++ test_challenge_code/utils.py
@@ -1,3 +1,8 @@
+import os
 import hdf5storage
+
 def save_matv73(mat_name, var_name, var):
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(mat_name), exist_ok=True)
+    
     hdf5storage.savemat(mat_name, {var_name: var}, format='7.3', store_python_metadata=True)
